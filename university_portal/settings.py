2dd2bf1bf62b4bae3974f679f901c201ed8e4973
Eric Danso <aericdanso98@gmail.com>
2025-03-25 22:51:09 +0800

    Database Update

--- university_portal/settings.py
+++ university_portal/settings.py
@@ -99,12 +99,12 @@ WSGI_APPLICATION = "university_portal.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 
 
 # Password validation
@@ -156,17 +156,17 @@ AUTHENTICATION_BACKENDS = [
     'django.contrib.auth.backends.ModelBackend',
 ]
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'dbname',
-#         'USER': 'mysqlusername',
-#         'PASSWORD': 'mysqlpassword',
-#         'HOST': 'localhost',  # Or your MySQL server IP
-#         'PORT': '3306',       # MySQL default port
-#         'OPTIONS': {
-#             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
-#             'charset': 'utf8mb4',
-#         },
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'dbname',
+        'USER': 'mysqlusername',
+        'PASSWORD': 'mysqlpassword',
+        'HOST': 'localhost',  # Or your MySQL server IP
+        'PORT': '3306',       # MySQL default port
+        'OPTIONS': {
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
+            'charset': 'utf8mb4',
+        },
+    }
+}
